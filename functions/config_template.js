11411df3e16a4ee268c050f786e692c0d6bb5c69
zx25 <timwells018@gmail.com>
2022-09-03 21:53:44 +0100

    Update function

---
+++ functions/config_template.js
@@ -0,0 +1,14 @@
+
+// Rename config_template.js to config.js
+// Add api_keys to array as guuid's
+const config = {
+    apiKeys: [
+        "aaaaaaaa-1111-2222-3333-xxxxxxxxxxxx",
+        "bbbbbbbb-1111-2222-3333-yyyyyyyyyyyy",
+        "bbbbbbbb-1111-2222-3333-yyyyyyyyyyyy",
+    ]
+}
+
+module.exports = {
+    config
+}
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,64 @@
 const functions = require("firebase-functions");
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { config } = require("./config");
+
+const VERSION = "0.0.1";
+const API_KEY_NAME = "x-api-key"
+
+const unauthorized = (res) => res.status(401).send('unauthorized');
+const isApiKeyValid = (request,keyName,apiKeys) => {
+   const apiKey = request.header(keyName);
+   return (apiKey != undefined && 
+               apiKey != null && apiKey.length > 0) ? 
+                  apiKeys.includes(apiKey) : false;
+}
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+app.get('/version', (request, response) => {
+    response.send(VERSION);
+})
+
+app.get('/version2', (request, response) => {
+   response.send(VERSION);
+})
+
+// Expose Express API as a single Cloud Function:
+exports.xenx = functions.https.onRequest(app);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
 functions.logger.info("Hello logs!", {structuredData: true});
    response.send("Hello from Firebase!");
 });
+
