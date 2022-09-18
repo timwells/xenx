a0bfc7e6d04e26fcec2c0fd8ad6f019efca26f92
zx25 <timwells018@gmail.com>
2022-09-18 20:17:13 +0100

    Write Config and SmartMeterReader updates

---
+++ arduino/SmartMeterReader/SmartMeterReader.ino
@@ -0,0 +1,155 @@
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
+// https://www.youtube.com/watch?v=cTfjqNFAkZ8
+
+#define LED 2 // Define blinking LED pin
+#define DELAY  15000
+#define DELAY__5 50
+#define DELAY_5 500
+#define DELAY1 1000
+#define DELAY2 2000
+#define DELAY5 5000
+#define LIGHT_ON 350
+#define VERSION "1.0"
+
+#define MAX_WIFI_PROPERTY_DEFAULT 30
+#define MAX_API_SCHEME_LENGTH     6
+#define MAX_API_SUBDOMAIN_LENGTH  40
+#define MAX_API_DOMAIN_LENGTH     40
+#define MAX_API_SUBDIR_LENGTH     10
+#define MAX_API_KEY_LENGTH        40
+
+typedef struct { 
+  char ssid[MAX_WIFI_PROPERTY_DEFAULT];
+  char pwd[MAX_WIFI_PROPERTY_DEFAULT];
+} WIFI;
+
+typedef struct { 
+  char scheme[MAX_API_SCHEME_LENGTH];
+  char subdomain[MAX_API_SUBDOMAIN_LENGTH];
+  char domain[MAX_API_DOMAIN_LENGTH];
+  char subdir[MAX_API_SUBDIR_LENGTH];
+  char apiKey[MAX_API_KEY_LENGTH];
+} SERVICE;
+
+typedef struct {
+  WIFI wifi;
+  SERVICE service;
+} CONFIG;
+
+
+#define ANALOG_PORT A0 /* ESP8266 Analog Pin ADC0 = A0 */
+
+int adcValue = 0;  /* Variable to store Output of ADC */
+CONFIG gConfig;
+String gAPI;
+
+void setup() {
+  pinMode(LED, OUTPUT); // Initialize the LED pin as an output
+  Serial.begin(115200);
+
+  // Read Config from EEPROM
+  memset(&gConfig, 0x0, sizeof(gConfig));
+  
+  EEPROM.begin(sizeof(CONFIG));
+  EEPROM.get(0, gConfig); 
+
+  gAPI = String(gConfig.service.scheme) + "://" 
+           + String(gConfig.service.subdomain) + "." 
+           + String(gConfig.service.domain) + "/" 
+           + String(gConfig.service.subdir);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(gConfig.wifi.ssid, gConfig.wifi.pwd);
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(DELAY_5);    
+    // Serial.println("ssid: " + String(gConfig.wifi.ssid) + ", pwd: " + String(gConfig.wifi.pwd));
+    // Serial.println("host: " + gAPI);  
+    // Serial.println("apiKey: " + String(gConfig.service.apiKey));  
+    pulseLed();
+  }
+
+  Serial.println("WiFi Connected");
+}
+
+void getVersion(String api) {
+  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+  client->setInsecure();  
+  HTTPClient https;
+
+  String versionAPI = api + "/version";
+  
+  if (https.begin(*client, versionAPI )) {
+    int httpCode = https.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      pulseLed();
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = https.getString();
+        Serial.println(String(payload));
+      }
+    } else {
+      Serial.println("GET error: " + String(https.errorToString(httpCode).c_str()));
+    }
+    https.end();
+  } else  {
+    Serial.println("[HTTPS] Unable to connect");
+  }
+}
+
+void postEvent(String api) {
+  std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+  client->setInsecure();  
+  
+  String eventAPI = api + "/v1/event";
+  HTTPClient https;
+  
+  if (https.begin(*client, eventAPI )) {
+    // Specify content-type header
+    https.addHeader("x-api-key", gConfig.service.apiKey);
+
+    int httpCode = https.POST("");
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      pulseLed();
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = https.getString();
+        Serial.println(String(payload));
+      }
+    } else {
+      Serial.println("POST error: " + String(https.errorToString(httpCode).c_str()));
+    }
+    https.end();
+  } else  {
+    Serial.println("Unable to connect");
+  }
+}
+
+void loop() {
+  // getVersion(gAPI);    
+  postEvent(gAPI);
+  delay(DELAY5);
+  
+  /* Read the Analog Input value */
+  // adcValue = analogRead(ANALOG_PORT);
+  // Serial.println(adcValue);
+  /*
+  if(adcValue > LIGHT_ON) {
+    Serial.print("ADC Value = ");
+    Serial.println(adcValue);
+    postPulse(XENX_API_PULSE);
+  } 
+  */ 
+}
+
+void pulseLed() {
+  digitalWrite(LED, LOW);   // Turn the LED on (Note that LOW is the voltage level)
+  delay(DELAY__5);          // Wait ...
+  digitalWrite(LED, HIGH);  // Turn the LED off by making the voltage HIGH
+}
---
+++ arduino/SmartMeterReader/writeConfig/writeConfig.ino
@@ -0,0 +1,101 @@
+#include <EEPROM.h>
+
+#define _XENX_API_SCHEME       "https"
+#define _XENX_API_SUBDOMAIN    "xx-xxxxxxxx-xxxx-xxxxx"
+#define _XENX_API_DOMAIN       "cloudfunctions.net"
+#define _XENX_API_SUBDIR       "xxxx"
+#define _XENX_API_VERSION_PATH "version"
+#define _XENX_API_PULSE_PATH   "v1/event"
+#define _XENX_API_KEY          "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+#define WIFI_SSID             "vodafoneA8E74D"
+#define WIFI_PWD              "MXdmFfNfGbEetCme"
+
+#define MAX_WIFI_PROPERTY_DEFAULT 30
+
+#define MAX_API_SCHEME_LENGTH     6
+#define MAX_API_SUBDOMAIN_LENGTH  40
+#define MAX_API_DOMAIN_LENGTH     40
+#define MAX_API_SUBDIR_LENGTH     10
+#define MAX_API_KEY_LENGTH        40
+
+#define WR_DATA_MODE 0
+#define RD_DATA_MODE 1
+
+typedef struct { 
+  char ssid[MAX_WIFI_PROPERTY_DEFAULT];
+  char pwd[MAX_WIFI_PROPERTY_DEFAULT];
+} WIFI;
+
+typedef struct { 
+  char scheme[MAX_API_SCHEME_LENGTH];
+  char subdomain[MAX_API_SUBDOMAIN_LENGTH];
+  char domain[MAX_API_DOMAIN_LENGTH];
+  char subdir[MAX_API_SUBDIR_LENGTH];
+  char apiKey[MAX_API_KEY_LENGTH];
+} SERVICE;
+
+typedef struct {
+  WIFI wifi;
+  SERVICE service;
+} CONFIG;
+
+CONFIG gConfig;
+
+unsigned int gMode = WR_DATA_MODE;
+// unsigned int gMode = RD_DATA_MODE;
+
+void setup() {
+  // Start the serial output
+  Serial.begin(115200); 
+  Serial.println("Setup....");
+
+  if(WR_DATA_MODE == gMode) {
+    EEPROM.begin(sizeof(CONFIG));
+
+    // Clear Structure
+    memset(&gConfig, 0x0, sizeof(gConfig));
+
+    // Setup Wifi Data
+    strcpy(gConfig.wifi.ssid,WIFI_SSID);
+    strcpy(gConfig.wifi.pwd,WIFI_PWD);
+
+    // Setup Host Data
+    strcpy(gConfig.service.scheme,XENX_API_SCHEME);
+    strcpy(gConfig.service.subdomain,XENX_API_SUBDOMAIN);
+    strcpy(gConfig.service.domain,XENX_API_DOMAIN);
+    strcpy(gConfig.service.subdir,XENX_API_SUBDIR);  
+    strcpy(gConfig.service.apiKey,XENX_API_KEY);
+
+    // Write data to array in ram 
+    EEPROM.put(0, gConfig); 
+    // Write data from ram to flash memory. 
+    // Do nothing if there are no changes to EEPROM data in ram
+    EEPROM.commit();
+    EEPROM.end();
+  }
+}
+
+CONFIG rConfig;
+void loop() {
+  EEPROM.begin(sizeof(CONFIG));
+
+  gMode = RD_DATA_MODE;
+  
+  switch(gMode) {
+    case RD_DATA_MODE: {
+      memset(&rConfig, 0x0, sizeof(rConfig));
+      EEPROM.get(0, rConfig); // Read Config
+      
+      Serial.println("ssid: " + String(rConfig.wifi.ssid) + ", pwd: " + String(rConfig.wifi.pwd));
+      String api = String(rConfig.service.scheme) + "://" 
+                    + String(rConfig.service.subdomain) + "." 
+                    + String(rConfig.service.domain) + "/" 
+                    + String(rConfig.service.subdir);
+      Serial.println("host: " + api);  
+      Serial.println("apiKey: " + String(rConfig.service.apiKey));  
+    } break;
+    default:
+      Serial.println("Do Nothing!");      
+  }      
+  delay(5000);
+}
